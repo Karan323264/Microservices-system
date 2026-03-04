8212a884277e4c9ac273949cdee13dab387968cf
Karan323264 <karankkchintu@gmail.com>
2026-03-04 18:31:29 +0530

    docker compose and properties scale (postgreSql)

--- Spring_CompanyApplication/src/main/java/com/bound/test/services/CompanyService.java
+++ Spring_CompanyApplication/src/main/java/com/bound/test/services/CompanyService.java
@@ -4,7 +4,6 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
-
 import com.bound.test.dtos.CompanyDto;
 import com.bound.test.entity.Company;
 import com.bound.test.mapper.CompanyMapper;
